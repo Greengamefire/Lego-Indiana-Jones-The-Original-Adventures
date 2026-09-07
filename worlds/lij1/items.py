fd19ad147e1f82e5004087df315c5509f74c8e94
spencer2585 <spencer2585@gmail.com>
2026-09-07 09:26:14 -0400

    Make Item Creation support later expansions

--- worlds/lij1/items.py
+++ worlds/lij1/items.py
@@ -9,13 +9,16 @@ def get_item_name_to_id():
     return item_table
 
 #creates a singular item with all needed information
-def create_item(name: str, world) -> Item:
-    return Item(name, ItemClassification.progression, world.item_name_to_id[name], world.player)
+def create_item(name: str, type:str, world) -> Item:
+    if type == "Character":
+        return Item(name, ItemClassification.progression, CHARACTERS[name].id, world.player)
 
 def create_items(world):
     itempool: list[Item] = []
-    for item, Itemid in world.item_name_to_id.items():
-        itempool.append(create_item(item, world))
+    for character, data in CHARACTERS.items():
+        item = create_item(character, "Character", world)
+        itempool.append(item)
+
 
     world.multiworld.itempool += itempool
 
